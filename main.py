dc2294f196960d881a556bcd3cf601f0ed93e636
Zeina Hossamuldeen <zeinahossameldin0@gmail.com>
2026-09-07 00:29:24 +0300

    Add project files to Zeina branch

---
+++ main.py
@@ -0,0 +1,23 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
+
+model = OllamaLLM(model="llama3.2")
+
+template = """
+You are an expert in answering questions about a pizza restaurant
+Here are some relevant reviews: {reviews}
+Here is the question to answer: {question}
+"""
+prompt = ChatPromptTemplate.from_template(template)
+chain = prompt | model
+
+while True:
+    question = input("Ask your question or type quit to leave: ")
+    print("\n\n")
+    if question == "quit":
+        break
+    
+    reviews = retriever.invoke(question)
+    result = chain.invoke({"reviews": reviews, "question": question})
+    print(result)
